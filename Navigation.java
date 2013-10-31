7a25b0d552bf3cd30ba046abb83a82b173a0b8d0
connorwstein <Connor@vpn166013.wireless.mcgill.ca>
2013-10-31 16:26:43 -0400

    First Outline of Navigation Class

--- Navigation.java
+++ Navigation.java
@@ -1,7 +1,70 @@
 
 public class Navigation {
-	private TwoWheeledRobot robo;
+	//Properties of navigation
+	private TwoWheeledRobot robo; 
 	private Odometer odo;
+	private BlockDifferentiator bd;
+	
+	//Variables
+	private boolean hasBlock=false;
+	
+	//Constants
+	private final double TURNTO_THRESHOLD=1.0;
+	
+	//Constructor
+	public Navigation(TwoWheeledRobot robo, Odometer odo, BlockDifferentiator bd){
+		this.robo=robo;
+		this.odo=odo;
+		this.bd=bd;	
+		
+	}
+	
+	//Travel to method
+	public void travelTo(boolean hasBlock, double x, double y){
+		if(hasBlock){
+			//Code for navigating straight to green/red zone
+		}
+		else{
+				//travel to (x,y) coordinate passed
+		}		
+	}
+	
+	//Turn to method
+	public void turnTo(double angle, boolean stop) { 
+        
+        double error = angle - odo.getAng(); 
+    
+        while (Math.abs(error) > TURNTO_THRESHOLD) { 
+               
+    
+            error = angle - odo.getAng(); 
+    
+            if (error < -180.0) { //rotate clockwise at speed defined in Two Wheeled Robot
+                robo.rotateClockwise();
+            } else if (error < 0.0) { //rotate counter clockwise at speed defined in Two Wheeled Robot
+                robo.rotateClockwise();
+            } else if (error > 180.0) { //rotate counter clockwise at speed defined in Two Wheeled Robot
+                robo.rotateClockwise();
+            } else { //rotate clockwise at speed defined in Two Wheeled Robot
+                robo.rotateClockwise();
+            } 
+        } 
+    
+        if (stop) { 
+            robo.stopMotors();} 
+           
+    }
+	
+	//Avoid obstacle method
+	public void avoidObstacle(){
+		
+	}
+	
+	//Investigate block
+	public void investigateBlock(){
+		
+	}
+	
 	
 
 }
