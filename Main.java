3ad30bc5edcf5f5f51cbd5ed669cce1679cf1247
Bernard <Bernard.kaminski@mail.mcgill.ca>
2013-11-03 16:56:22 -0500

    update

--- Main.java
+++ Main.java
@@ -6,8 +6,15 @@ public class Main {
 	
 	public static void main(String[] args) 
 	{
-		//UltrasonicScannerTest.main();
-		LightSensorTest.main();
+		//uncomment to run the scanner test
+		UltrasonicScannerTest.main();
+		
+		
+		//uncomment this for light sensor test
+		//LightSensorTest.main();
+		
+		//uncomment this for ultrasonic poller test
+		//UltrasonicPollerTest.main();
 	}
 
 }
